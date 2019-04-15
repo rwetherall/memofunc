23ca39d802eec9032ce04762010b31977029f27a
rwetherall <roy.wetherall@alfresco.com>
2019-04-15 10:40:00 +1000

    Merge memory storage into cache

--- R/cache.R
+++ R/cache.R
@@ -1,68 +1,24 @@
 library(digest)
 
-#' # Envirnoment used to store the different cache types
-#' cache.env <- new.env(parent=emptyenv())
-#'
-#' ##
-#' # Create cache type key from cache type
-#' #
-#' cacheTypeKey <- function (type) paste("type", type, sep=".")
-#'
-#' ##
-#' #' @title
-#' #' Adds a cache type to those available
-#' #' @description
-#' #' Adds a cache type to the registry of those available for initialisation.
-#' #'
-#' #' A cache type is defined as a function that retuns a list of functions bound to a specific cache name as follows:
-#' #' * set(key, value) - sets a value for a key, overwrites any existing values at that key
-#' #' * get(key) - gets the value of a key, NULL otherwise
-#' #' * unset(key) - unsets value of a key
-#' #' * has(key) - true if the key has a value, false otherwise
-#' #' * clear() - clears all values
-#' #' @param type name of the cache type
-#' #' @param f function that returns a list of functions bound to a cache name
-#' #' @return
-#' #' list of functions that can be used to manipulate the named cache
-#' #' @examples
-#' #'
-#' #' @export
-#' ##
-#' addCacheType <- function(type, f) {
-#'
-#'   # register the cache type
-#'   assign(cacheTypeKey(type), f, envir=cache.env)
-#' }
-
 ##
 #' @title
 #' Get a cache
 #' @description
 #' Gets a cache of the type specified taking into account any context provided
-#' @param storage cache storage, defaults to memory storage if not specified
 #' @param algo hasing algorithm which defaults to sha1
 #' @return
 #' A cache that can be used to store values
 #' @export
 ##
-cache <- function (storage = NULL, algo="sha1") {
+cache <- function (algo="sha1") {
 
-  # check passed storage
-  if (is.null(storage)) {
-    # Use memory storage if none specified
-    storage <- memoryStorage()
-  } else {
-    # TODo verify that the passed storage is valid
-  }
+  # environment used as memory storage
+  memoryStorage <- new.env(parent=emptyenv())
 
-  #
   # Hash function
-  #
   hash <- function(value) digest::digest(value, algo=algo)
 
-  #
   # Helper to create hash from function and args details.
-  #
   hashFunctionCall <- function(name, formals, args) {
 
     # get default args
@@ -83,11 +39,24 @@ cache <- function (storage = NULL, algo="sha1") {
     hash(c(name, unlist(args.set, use.names = FALSE)))
   }
 
-  # verify the cache type
- #if (!exists(cacheTypeKey(type), envir=cache.env)) stop(paste("Undefined memo cache type:", type))
+  # Set value for given key
+  set <- function(key, value) assign(key, value, envir=memoryStorage)
+
+  # get value with key
+  get <- function(key) if (exists(key, envir=memoryStorage)) base::get(key, envir=memoryStorage) else NULL
+
+  # unset value with key
+  unset <- function(key) if (exists(key, envir=memoryStorage)) rm(list=c(key), envir=memoryStorage)
+
+  # has value with key
+  has <- function(key) exists(key, envir=memoryStorage)
+
+  # clear all
+  clear <- function() rm(list=base::ls(memoryStorage), envir=memoryStorage)
+
+  # list cache contents
+  ls <- function() base::ls(memoryStorage)
 
   # combine cache and storage features
-  c(
-    storage,
-    list(algo = algo, hash = hash, hashFunctionCall = hashFunctionCall))
+  list(algo = algo, hash = hash, hashFunctionCall = hashFunctionCall, set = set, get = get, unset = unset, has = has, clear = clear, ls=ls)
 }

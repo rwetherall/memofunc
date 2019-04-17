fdb3371fc7ea46294dec2584fc874f8ae94cf62d
rwetherall <rwetherall@gmail.com>
2019-04-17 12:57:20 +1000

    Test commit

--- R/filestore.R
+++ R/filestore.R
@@ -1,15 +1,24 @@
 filestore <- function (dir=".") {
 
+  ##
+  # TODO documentation
+  ##
   before <- function(cacheId) {
     if (!dir.exists(dir)) dir.create(dir)
     if (!dir.exists(file.path(dir, cacheId))) dir.create(file.path(dir, cacheId))
   }
 
+  ##
+  # TODO documentation
+  ##
   write <- function (cacheId, key, value) {
     before(cacheId)
     saveRDS(value, file = file.path(dir, cacheId, key))
   }
 
+  ##
+  # TODO documentation
+  ##
   read <- function (cacheId, key) {
 
     # todo handle things not being there!

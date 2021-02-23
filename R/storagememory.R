c4e8679b076b8c961bce9fc2c8ed31d49e0a1ae0
Roy Wetherall <rwetherall@gmail.com>
2021-02-23 20:35:07 +1100

    Remove default storage methods and replace with specific named memory implementations

--- R/storagememory.R
+++ R/storagememory.R
@@ -14,7 +14,7 @@ storage.memory.class = "memory"
 #' @inherit storage.init
 #' @export
 ##
-storage.init.default <- function (storage.type = storage.memory.class, ...) 
+storage.init.memory <- function (storage.type = storage.memory.class, ...) 
   list(
     env = new.env(parent=emptyenv())
   ) %>% `class<-`(c(storage.root.class, storage.memory.class))
@@ -24,7 +24,7 @@ storage.init.default <- function (storage.type = storage.memory.class, ...)
 #' @inherit storage.set
 #' @export
 ##
-storage.set.default <- function (storage, key, value){
+storage.set.memory <- function (storage, key, value){
   assign(key, value, envir=storage$env)
   invisible(storage)
 }
@@ -34,7 +34,7 @@ storage.set.default <- function (storage, key, value){
 #' @inherit storage.get
 #' @export
 ##
-storage.get.default <- function(storage, key) 
+storage.get.memory <- function(storage, key) 
   if (exists(key, envir=storage$env)) base::get(key, envir=storage$env) else NULL
 
 ##
@@ -42,7 +42,7 @@ storage.get.default <- function(storage, key)
 #' @inherit storage.unset
 #' @export
 ##
-storage.unset.default <- function(storage, key) {
+storage.unset.memory <- function(storage, key) {
   if (exists(key, envir=storage$env)) rm(list=c(key), envir=storage$env)
   invisible(storage)
 }
@@ -52,14 +52,14 @@ storage.unset.default <- function(storage, key) {
 #' @inherit storage.has
 #' @export
 ##
-storage.has.default <- function(storage, key) exists(key, envir=storage$env)
+storage.has.memory <- function(storage, key) exists(key, envir=storage$env)
 
 ##
 #' @title Clear the memory store.
 #' @inherit storage.clear
 #' @export
 ##
-storage.clear.default <- function(storage) {
+storage.clear.memory <- function(storage) {
   rm(list=base::ls(storage$env), envir=storage$env)
   invisible(storage)
 }
